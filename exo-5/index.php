42c4269d1faef1dd2da9db71bc8e101d0054e563
Yoann <scellier.yoann@gmail.com>
2024-05-07 11:25:08 +0200

    Exercice AJAX - Fetch API + REGEX email + limitation nombre caractère + couleur champ formulaire

--- exo-5/index.php
+++ exo-5/index.php
@@ -17,67 +17,66 @@
         <label for="message">Message :</label><br>
         <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
 
-        <!-- Ajoute d'autres champs au besoin -->
-
         <button type="submit">Envoyer</button>
-    </form>
 
-    <!-- Script pour la gestion des données du formulaire -->
-    <script>
-        // Lien API pour l'envoi des données du formulaire
-        // API : https://reqres.in/api/users
-        // Code Regex : /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        <script>
+            // API : https://reqres.in/api/users
+            // Code Regex : /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-        function isValidEmail(email) {
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            return emailRegex.test(email);
-        }
-
-        function validateForm(name, email, message) {
-            if(!name || !email || !message) {
-                alert('Veuillez remplir les champs vide')
-                return false;
+            function isValidEmail(email) {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return emailRegex.test(email);
             }
 
-            if(!isValidEmail(email)) {
-                alert('Veuillez remplir une adresse mail valide')
-                return false;
-            }
-            return true;
-        }
-
-        document.getElementById('userForm').addEventListener('submit', submitForm);
-
-        function submitForm(event) {
-            event.preventDefault();
-
-            const formData = new FormData(event.target);
-            const name = formData.get('name');
-            const email = formData.get('email');
-            const message = formData.get('message');
-            const userData = {name, email, message};
-
-            fetch('https://reqres.in/api/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userData)
-            })
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error('Erreur')
+            function validateForm(name, email, message) {
+                if(!name || !email || !message) {
+                    alert('Champ invalide')
+                    return false;
                 }
-                return response.json();
-            })
-            .then(user => {
-                console.log('Utilisateur trouvé!', user);
-            })
-            .catch(error => {
-                console.error('Une erreur est survenu...', error);
-            })
-        }
 
-</script>
+                if(!isValidEmail(email)) {
+                    alert('Champ email invalide')
+                    return false;
+                }
+                return true;
+            }
+
+            document.getElementById('userForm').addEventListener('submit', submitForm);
+
+            function submitForm(event) {
+                event.preventDefault();
+
+                const formData = new FormData(event.target);
+                const name = formData.get('name');
+                const email = formData.get('email');
+                const message = formData.get('message');
+                const userData = {name, email, message};
+
+                if (!validateForm(name, email, message)) {
+                    return;
+                }
+
+                fetch ('https://reqres.in/api/users', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'                    
+                    },
+                    body: JSON.stringify(userData)
+                })
+                .then(response => {
+                    if(!response.ok) {
+                        throw new Error('Erreur')
+                    }
+                    return response.json();
+                })
+                .then(user => {
+                    console.log('Utilisateur enregistrer', user);
+                })
+                .catch(error => {
+                    console.error('Erreur survenue', error);
+                })
+            }
+        </script>
+    </form>
 </body>
 </html>
---
+++ exo-6/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'envoi de données</title>
+    <link rel="stylesheet" href="styles.css"> 
+    <!-- Ajout du lien CSS pour les couleurs de validation ou de inavlidation du formulaire -->
+</head>
+<body>
+    <h2>Formulaire d'envoi de données</h2>
+
+    <!-- Début du formulaire -->
+    <form id="userForm">
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="name" required><br><br>
+        
+        <label for="email">E-mail :</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="message">Message :</label><br>
+        <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
+        <!-- Ajout de "requried" en fin de l'input et textarea pour signaler que le champ dois être rempli. -->
+
+        <button type="submit">Envoyer</button>
+    </form>
+    <!-- Fin du Formulaire -->
+
+    <!-- Emplacement pour le script -->
+    <script>
+        // API : https://reqres.in/api/users
+        // Code Regex : /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+        function isValidEmail(email) {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+
+        function validateForm(name, email, message) {
+            if(!name || !email || !message) {
+                alert('Une erreur est survenue, veuillez remplir les champs du formulaire.')
+                return false;
+            }
+
+            if(!isValidEmail(email)) {
+                alert('Attention email invalide.')
+                return false;
+            }
+
+            if(name.length > 50) {
+                alert('Attention votre nom dépasse les 50 caractères.')
+                return false;
+            }
+
+            if(message.length > 500) {
+                alert('Attention votre message dépasse les 500 caractères.')
+                return false;
+            }
+            return true;
+        }
+
+        document.getElementById('userForm').addEventListener('submit', submitForm);
+
+        function submitForm(event) {
+            event.preventDefault();
+
+            const formData = new FormData(event.target);
+            const name = formData.get('name');
+            const email = formData.get('email');
+            const message = formData.get('message');
+            const userData = {name, email, message};
+
+            if(!validateForm(name, email, message)){
+                return false;
+            }
+
+            fetch('https://reqres.in/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            })
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('Une erreur est survenue!')
+                }
+                return response.json();
+            })
+            .then(user => {
+                alert('Vous avez bien était enregistrer!')
+                console.log('Utilisateur enregistrer.');
+            })
+            .catch(error => {
+                alert('Vous avez une erreur dans l\'un des champs du formulaire.')
+                console.error('Erreur formulaire.');
+            })
+        }
+    </script>
+</body>
+</html>
